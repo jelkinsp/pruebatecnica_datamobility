ce614612e260fcf2c519dc1035548a3836c804b3
jose <jlluengoramos@gmail.com>
2021-11-07 17:14:46 +0100

    0.1.0: the map is added with the interpretation of the data

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-import glob
-import json
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
-import plotly.express as px
-from dash.dependencies import Input, Output
+
+# from display.graphs import display_choropleth, display_historygram
+from utils.utils import get_dataframe
 
 app = dash.Dash(__name__)
 
+df, geojson = get_dataframe()
+
+
 app.layout = html.Div([
-    html.P("Data Mobility:"),
+    html.H2("Data Mobility:"),
+    # dcc.Graph(
+    #     id='choropleth_graph',
+    #     figure=display_choropleth(df, geojson)
+    # ),
+    # html.Div(id="click_result"),
+    # generate_table(df),
+    # dcc.Graph(
+    #     id='histogram-mobility',
+    #     figure=display_historygram(df)
+    # )
 ])
 
 if __name__ == '__main__':
